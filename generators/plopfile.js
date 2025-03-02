4b8cb3f1948ccbf3af44b40cbf6b13df6725f1e0
João Souza <ms.joao.felipe@gmail.com>
2025-03-02 17:51:08 -0300

    chore: add file generator with Plop

---
+++ generators/plopfile.js
@@ -0,0 +1,25 @@
+export default function (plop) {
+  plop.setGenerator("entities", {
+    description: "Create a new entity",
+    prompts: [{ type: "input", name: "name", message: "What is the name of the entity?" }],
+    actions: [...entitiesCreations],
+  });
+}
+
+const entitiesCreations = [
+  {
+    type: "add",
+    path: "../src/slices/{{camelCase name}}/entities/index.ts",
+    templateFile: "./templates/entities/index.ts.hbs",
+  },
+  {
+    type: "add",
+    path: "../src/slices/{{camelCase name}}/entities/{{camelCase name}}Entity.ts",
+    templateFile: "./templates/entities/DomainEntity.ts.hbs",
+  },
+  {
+    type: "add",
+    path: "../src/slices/{{camelCase name}}/entities/{{camelCase name}}Entity.spec.ts",
+    templateFile: "./templates/entities/DomainEntity.spec.ts.hbs",
+  },
+];
