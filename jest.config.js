2e917b7d29e62d775e44e23ffa5f8a65b5a045dd
João Souza <ms.joao.felipe@gmail.com>
2025-03-02 12:06:54 -0300

    chore: initial project setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "ts-jest",
+  bail: 1, 
+  clearMocks: true,
+  collectCoverage: true,
+  setupFiles: ["dotenv/config"],
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["src/**/*.{ts}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    "^·+\\.ts$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  }
+}
